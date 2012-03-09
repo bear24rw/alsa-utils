09afe916cbf80641da98347970ac34a60e3cb45a
Max Thrun <bear24rw@gmail.com>
2012-03-08 20:01:06 -0500

    removed background color

--- alsamixer/colors.c
+++ alsamixer/colors.c
@@ -51,12 +51,13 @@ void init_colors(int use_color)
 {
 	if (!!has_colors() == !!use_color) {
 		start_color();
+        use_default_colors();
 
-		init_pair(1, COLOR_CYAN, COLOR_BLACK);
-		init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+		init_pair(1, COLOR_CYAN, -1);
+		init_pair(2, COLOR_YELLOW, -1);
 		init_pair(3, COLOR_WHITE, COLOR_GREEN);
-		init_pair(4, COLOR_RED, COLOR_BLACK);
-		init_pair(5, COLOR_WHITE, COLOR_BLACK);
+		init_pair(4, COLOR_RED, -1);
+		init_pair(5, COLOR_WHITE, -1);
 		init_pair(6, COLOR_WHITE, COLOR_BLUE);
 		init_pair(7, COLOR_RED, COLOR_BLUE);
 		init_pair(8, COLOR_GREEN, COLOR_GREEN);
